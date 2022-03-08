75155e3018a9c7ecf8b86e7febc75fb42dbab5e3
Tanise Santos <tanise234@gmail.com>
2022-03-07 23:55:07 -0300

    par e ímpar

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/impar.js
@@ -0,0 +1,10 @@
+const a = 9;
+const b = 3;
+const c = 45;
+
+if ((a % 2 === 1) ||(b % 2 === 1) ||(c % 2 === 1)){
+    console.log(true);
+}
+else {
+    console.log(false);
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/par.js
@@ -0,0 +1,10 @@
+const a = 9;
+const b = 3;
+const c = 45;
+
+if ((a % 2 === 0) ||(b % 2 === 0) ||(c % 2 === 0)){
+    console.log(true);
+}
+else {
+    console.log(false);
+}
