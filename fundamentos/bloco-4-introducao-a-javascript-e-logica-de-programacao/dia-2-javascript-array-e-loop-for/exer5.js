82625444288b1b0564267d3486891638e4a1d870
Tanise Santos <tanise234@gmail.com>
2022-03-08 19:20:39 -0300

    exer7 feito e exer5 alterado (inicio biggest)

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exer5.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exer5.js
@@ -1,6 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let biggest = 0;
+let biggest = numbers[0];
 
 for (let index = 0; index < numbers.length; index++) {
     if (biggest < numbers[index]){
