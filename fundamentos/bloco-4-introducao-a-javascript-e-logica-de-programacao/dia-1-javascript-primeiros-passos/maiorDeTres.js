a9e55e11d5b17da63e9de906347dc3463d2dbf18
Tanise Santos <tanise234@gmail.com>
2022-03-07 23:03:38 -0300

    maior de três

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/maiorDeTres.js
@@ -0,0 +1,13 @@
+let num1 = 8;
+let num2 = 3;
+let num3 = 7;
+
+if (num1 >= num2 && num1 >= num3){
+    console.log(num1)
+}
+else if (num1 <  num2 && num2 >= num3){
+    console.log(num2)
+}
+else {
+    console.log(num3)
+}
