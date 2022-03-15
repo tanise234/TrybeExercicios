49f400f84ccc10fbbcb3c884ec61547a297c40a8
Tanise Santos <tanise234@gmail.com>
2022-03-14 22:21:46 -0300

    parte 1 concluída

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,5 @@
+// function tagP() {
+//     document.getElementsByTagName("p") = 'Daqui a dois anos eu quero estar formada pela Trybe, trabalhando em um empresa que combine muito comigo e com um salário que me permita viver bem e realizar meus sonhos.';
+// }
+
+// tagP;
