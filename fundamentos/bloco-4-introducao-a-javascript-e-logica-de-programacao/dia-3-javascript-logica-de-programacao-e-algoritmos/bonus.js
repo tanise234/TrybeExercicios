c8de105d0c4e6a191f057fa97dbf4531a449e46f
Tanise Santos <tanise234@gmail.com>
2022-03-18 19:20:06 -0300

    exer 1, 2, 3 resolvidos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
@@ -1,4 +1,4 @@
-// exer 1 - quadrado
+// // exer 1 - quadrado
 // let n = 5;
 // for (let i = 0; i < n; i += 1) {
 //     let display = '';
@@ -8,7 +8,7 @@
 //     console.log(display);
 // }
 
-// exer 2 - triângulo à esquerda
+// // exer 2 - triângulo à esquerda
 // let n = 5;
 // for (let i = 0; i < n; i += 1) {
 //     let display = '';
@@ -19,17 +19,50 @@
 // }
 
 
-// exer 3 - triângulo à direita
+// // exer 3 - triângulo à direita
+// let n = 5;
+// for (let i = 0; i < n; i += 1) {
+//     let display = '';
+//     for (let j = 0; j < n; j += 1) {
+//         if (n - j <= i + 1) {
+//             display = display + '*';
+//         } else {
+//             display = display + ' ';
+//         }
+//     }
+//     console.log(display);
+// }
+
+// exer 4 - árvore de natal
 let n = 5;
 for (let i = 0; i < n; i += 1) {
     let display = '';
     for (let j = 0; j < n; j += 1) {
-        if (n - j <= i + 1) {
-            display = display + '*';
-        } else {
-            display = display + ' ';
-        }
+        display = display + '*';
     }
     console.log(display);
 }
 
+// exer 5 - contorno da árvore de natal
+
+
+// // exer 6 - verificar se é primo
+// let limit = 50;
+
+// // verificar os valores começando pelos maiores
+// for (let index = limit; index > 0; index -= 1) {
+//     let primo = true;
+
+//     // verificar se existem divisores começando pelo 2 e indo até a metade do número
+//     for (let divider = 2; divider <= index / 2; divider += 1) {
+//         if (index % divider === 0) {
+//             primo = false;
+//             break;
+//         }
+//     }
+
+//     if (primo === true) {
+//         console.log(index + " é o maior primo!");
+//         break;
+//     }
+// }
