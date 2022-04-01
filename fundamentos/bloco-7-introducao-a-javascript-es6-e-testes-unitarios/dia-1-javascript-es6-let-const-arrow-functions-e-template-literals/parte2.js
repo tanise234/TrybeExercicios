254adceb71286c7f41b596953441f3c8d41dfc8a
Tanise Santos <tanise234@gmail.com>
2022-04-01 20:28:15 -0300

    exer2 feito

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2.js
@@ -0,0 +1,31 @@
+// const fatorial = num => {
+//     for (let i = num -1; i > 0; i -= 1){
+//         num *= i;
+//     }
+//     return num;
+// }
+
+// console.log(fatorial(5));
+
+// ====================================================
+
+// const fatorial = num => num === 1 ? 1: num * fatorial(num-1);
+
+// console.log(fatorial(4));
+
+// ====================================================
+
+const longestWord = (phrase) => {
+  const words = phrase.split(" ");
+  let longest = words[0];
+  for (let i = 1; i < words.length; i += 1) {
+    words[i].length > longest.length
+      ? (longest = words[i])
+      : (longest = longest);
+  }
+  return longest;
+};
+
+console.log(
+  longestWord("Antônio foi no banheiro e não sabemos o que aconteceu")
+);
