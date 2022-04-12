7014227d9a6c75dec90cc26e1a93e16830e86b29
Tanise Santos <tanise234@gmail.com>
2022-04-12 03:02:17 -0300

    exercício 5 feito

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise4.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise4.js
@@ -62,7 +62,7 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
-let oldBooksOrdered = books
+const oldBooksOrdered = books
   .filter((book) => 2022 - book.releaseYear > 60)
   .sort((a, b) => a.releaseYear - b.releaseYear);
 console.log(oldBooksOrdered);
