6cd8ed7fa28bc6ac4a9d4bab87583eb53ebf5337
Tanise Santos <tanise234@gmail.com>
2022-05-05 23:48:57 -0300

    .

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio10_3/src/Content.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const conteudos = [
+  {
+    conteudo: 'High Order Functions',
+    bloco: 8,
+    status: 'Aprendido',
+  },
+  {
+    conteudo: 'Composicao de Componentes',
+    bloco: 11,
+    status: 'Aprendendo',
+  },
+  {
+    conteudo: 'Composicao de Estados',
+    bloco: 12,
+    status: 'Aprenderei',
+  },
+  {
+    conteudo: 'Redux',
+    bloco: 16,
+    status: 'Aprenderei',
+  },
+];
+
+class Content extends React.Component {
+  render() {
+    return (
+      <p>
+        {conteudos.map(function (conteudo) {
+            const block = <section key={conteudo.conteudo}>
+            {`O conteúdo é: ${conteudo.conteudo}
+            Status: ${conteudo.status}
+            Bloco: ${conteudo.bloco}`}
+          </section>;
+          return block;
+        })}
+      </p>
+    );
+  }
+}
+
+export default Content;
