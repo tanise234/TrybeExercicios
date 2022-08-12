25b7d073ed28ad734f99e3a66f3545eb31f276dd
Tanise Santos <tanise234@gmail.com>
2022-08-12 18:30:29 -0300

    exec 2

---
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exec2.sql
@@ -0,0 +1,5 @@
+SELECT m.title, b.domestic_sales, b.international_sales
+FROM pixar.movies AS m
+INNER JOIN pixar.box_office AS b
+ON m.id = movie_id
+WHERE b.domestic_sales < b.international_sales;
