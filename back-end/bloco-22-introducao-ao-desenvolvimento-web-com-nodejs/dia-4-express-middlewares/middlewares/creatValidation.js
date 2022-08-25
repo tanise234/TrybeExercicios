cbf87d3c68d238d1e6987ecd127ab1fd460b44ef
Tanise Santos <tanise234@gmail.com>
2022-08-25 00:27:45 -0300

    creatValidation implementada

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-middlewares/middlewares/creatValidation.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-middlewares/middlewares/creatValidation.js
@@ -1,3 +1,11 @@
-const creatValidation = (req, res, next) => {next()};
+const creatValidation = (req, res, next) => {
+    const date = req.body.description.createdAt;
+    const format = /^(\d{2})\/(\d{2})\/(\d{4})$/;
+
+    if (!format.test(date)) {
+        return res.status(400).json({ "message": "O campo createdAt deve ter o formato \'dd/mm/aaaa\'." });
+    }
+    next();
+};
 
 module.exports = creatValidation;
