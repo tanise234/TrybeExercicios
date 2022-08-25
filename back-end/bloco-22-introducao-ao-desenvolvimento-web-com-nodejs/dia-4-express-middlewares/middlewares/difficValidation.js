139d1557c97b2f157f31d4e07a0b217418ea87d7
Tanise Santos <tanise234@gmail.com>
2022-08-24 23:58:57 -0300

    difficValidadtion implementada

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-middlewares/middlewares/difficValidation.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-middlewares/middlewares/difficValidation.js
@@ -1,3 +1,11 @@
-const difficValidation = (req, res, next) => {next()};
+const difficValidation = (req, res, next) => {
+    const difficulty = req.body.description.difficulty;
+    const classification = ['Fácil', 'Médio', 'Difícil'];
+
+    if (classification.every(level => level !== difficulty)) {
+        return res.status(400).json({ "message": "O campo difficulty deve ser \'Fácil\', \'Médio\' ou \'Difícil\'." });
+    }
+    next();
+};
 
 module.exports = difficValidation;
